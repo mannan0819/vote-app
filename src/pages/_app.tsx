a8dc2799d5cb6c7e06d2534083c1d4e1d2add8af
Mannan Arain <marain@rentsync.com>
2022-09-05 22:06:08 -0400

    fix vercel url

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,8 @@
-import { withTRPC } from '@trpc/next';
-import { AppType } from 'next/dist/shared/lib/utils';
-import superjson from 'superjson';
-import { AppRouter } from '../backend/router';
-import '../styles/globals.css';
+import { withTRPC } from "@trpc/next";
+import { AppType } from "next/dist/shared/lib/utils";
+import superjson from "superjson";
+import { AppRouter } from "../backend/router";
+import "../styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   return <Component {...pageProps} />;
@@ -14,9 +14,9 @@ export default withTRPC<AppRouter>({
      * If you want to use SSR, you need to use the server's full URL
      * @link https://trpc.io/docs/ssr
      */
-    const url = process.env.VERCEL_URL
-      ? `https://${process.env.VERCEL_URL}/api/trpc`
-      : 'http://localhost:3000/api/trpc';
+    const url = process.env.NEXT_PUBLIC_VERCEL_URL
+      ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api/trpc`
+      : "http://localhost:3000/api/trpc";
 
     return {
       url,
