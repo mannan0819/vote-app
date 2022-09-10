d922a00ac47fb7ba562e084462c2493427618fe6
Mannan Arain <marain@rentsync.com>
2022-09-10 14:29:55 -0400

    bug fix

--- src/pages/question/[id].tsx
+++ src/pages/question/[id].tsx
@@ -2,21 +2,20 @@ import { useRouter } from "next/router";
 import { trpc } from "../../utils/trpc";
 
 export default function QuestionId() {
-  const router = useRouter()
+  const router = useRouter();
   const id = router.query.id;
   if (typeof id !== "string") return null;
   const { data, isLoading } = trpc.useQuery(["question.getById", { id }]);
 
-  if (!data) return <div> NO QUESTION FOUND.</div>
+  if (!data) return <div> NO QUESTION FOUND.</div>;
   // console.log(data);
   return isLoading || !data ? (
     <div>Loading...</div>
   ) : (
     <div className="flex flex-col p-6">
       <div className="text-2xl text-red-600">Questions</div>
-      <p>{data.question}</p>
-      <div>
-      </div>
+      <p>{data.question?.question}</p>
+      <div></div>
     </div>
   );
 }
